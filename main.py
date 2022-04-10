ba7a1b77d7177af880f48d3005d61b89d672b137
cwebs <codylw2@illinois.edu>
2022-04-09 22:31:49 -0500

    load data edits, still need y vals

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
-import pandas as pd
-import sys
 import random
+
 import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torch.utils.data.dataset import random_split
 
 # set seed
 seed = 24
@@ -15,21 +17,26 @@ torch.manual_seed(seed)
 os.environ["PYTHONHASHSEED"] = str(seed)
 DATA_PATH = "./data"
 
-from torch.utils.data import Dataset
-from torch.utils.data.dataset import random_split
-from torch.utils.data import DataLoader
-
 
 class CustomDataset(Dataset):
     def __init__(self):
         # compile data
+        unq_codes = set()
+        unq_cats = set()
+
         df = pd.read_csv(os.path.join(DATA_PATH, 'DIAGNOSES_ICD.csv'))
         data_dict = dict()
         for row in df.to_dict('records'):
             try:
                 patient = int(row['SUBJECT_ID'])
                 visit = int(row['HADM_ID'])
-                code = (int(row['SEQ_NUM']), str(row['ICD9_CODE']))  # allows for sorting
+                code = row['ICD9_CODE']
+                if (len(code) < 5):
+                    code += '0' * (5 - len(code))
+
+                code_set = (int(row['SEQ_NUM']), code)  # allows for sorting
+                unq_codes.add(code)
+                unq_cats.add(code[0:3])
             except Exception as e:
                 # ignore rows that have any missing data
                 continue
@@ -38,17 +45,40 @@ class CustomDataset(Dataset):
                 data_dict[patient] = dict()
             if visit not in data_dict[patient].keys():
                 data_dict[patient][visit] = list()
-            data_dict[patient][visit].append(code)
+            data_dict[patient][visit].append(code_set)
+
+        code_map = dict()
+        rev_code_map = dict()
+        for idx, code in enumerate(sorted(unq_codes)):
+            code_map[code] = idx + 1
+            rev_code_map[idx + 1] = code
 
         data = list()
         for i_patient, patient in enumerate(data_dict.keys()):
             patient_list = list()
-            for j_visit, visit in enumerate(data_dict[patient].keys()):
-                codes = [code for seq, code in sorted(data_dict[patient][visit])]
+            if len(data_dict[patient].keys()) < 2:
+                continue  # filter out patients with less than two visits
+            for j_visit, visit in enumerate(sorted(data_dict[patient].keys())):
+                codes = [code_map[code] for seq, code in sorted(data_dict[patient][visit])]
                 patient_list.append(codes)
             data.append(patient_list)
 
+        num_visits = [len(patient) for patient in data]
+        num_codes = [len(visit) for patient in data for visit in patient]
+        self.num_patients = len(data)
+        self.max_num_visits = max(num_visits)
+        self.max_num_codes = max(num_codes)
         self.x = data
+
+        print('number of patients', self.num_patients)
+        print('number of visits', sum(num_visits))
+        print('average number of visits', sum(num_visits) / len(num_visits))
+        print('number of unique codes', len(unq_codes))
+        print('average number of codes per visit', sum(num_codes) / len(num_codes))
+        print('max number of codes per visit', self.max_num_codes)
+        print('number of unique categories', len(unq_cats))
+        print()
+        return
 
     def __len__(self):
         return len(self.x)
@@ -57,13 +87,12 @@ class CustomDataset(Dataset):
         return self.x[index]
 
 
-def collate_fn(data):
+def collate_fn(data, **kwargs):
     num_patients = len(data)
-    num_visits = [len(patient) for patient in data]
-    num_codes = [len(visit) for patient in data for visit in patient]
+    # print('kwargs', kwargs)
 
-    max_num_visits = max(num_visits)
-    max_num_codes = max(num_codes)
+    max_num_visits = kwargs.get('max_num_visits', max([len(patient) for patient in data]))
+    max_num_codes = kwargs.get('max_num_codes', max([len(visit) for patient in data for visit in patient]))
 
     x = torch.zeros((num_patients, max_num_visits, max_num_codes), dtype=torch.long)
     rev_x = torch.zeros((num_patients, max_num_visits, max_num_codes), dtype=torch.long)
@@ -94,8 +123,9 @@ def split_dataset(dataset):
     return train_dataset, val_dataset, test_dataset
 
 
-def load_data(train_dataset, val_dataset, test_dataset, collate_fn):
-    batch_size = 32
+def load_data(train_dataset, val_dataset, test_dataset, collate_fn, batch_size, **kwargs):
+    # print('loading data')
+    # collate = partial(collate_fn, [], kwargs)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, collate_fn=collate_fn)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=collate_fn)
@@ -103,7 +133,26 @@ def load_data(train_dataset, val_dataset, test_dataset, collate_fn):
 
 
 def train(model, train_loader, val_loader, n_epochs):
-    pass
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-4, weight_decay=1e-4)  # TODO: replace
+    criterion = nn.BCELoss()  # TODO: replace
+    eval_model = None  # TODO: replace
+
+    for epoch in range(n_epochs):
+        model.train()
+        train_loss = 0
+        for x, masks, rev_x, rev_masks, y in train_loader:
+            optimizer.zero_grad()
+            y_hat = model(x, masks, rev_x, rev_masks)
+            loss = criterion(y_hat, y)
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+        train_loss = train_loss / len(train_loader)
+        print('Epoch: {} \t Training Loss: {:.6f}'.format(epoch + 1, train_loss))
+        # p, r, f, roc_auc = eval_model(model, val_loader)
+        # print('Epoch: {} \t Validation p: {:.2f}, r:{:.2f}, f: {:.2f}, roc_auc: {:.2f}'
+        # .format(epoch + 1, p, r, f, roc_auc))
 
 
 def test():
@@ -111,6 +160,18 @@ def test():
 
 
 if __name__ == '__main__':
+    params = {
+        'batch_size': 32,
+        'num_epochs': 10
+    }
+
     dataset = CustomDataset()
     train_dataset, val_dataset, test_dataset = split_dataset(dataset)
-    train_loader, val_loader, test_loader = load_data(train_dataset, val_dataset, test_dataset, collate_fn)
+
+    kwargs = {'max_num_visits': dataset.max_num_visits, 'max_num_codes': dataset.max_num_codes}
+    train_loader, val_loader, test_loader = load_data(train_dataset, val_dataset, test_dataset,
+                                                      collate_fn, params['batch_size'], **kwargs)
+
+    # model = nn.RNN(dataset.max_num_codes, 128, num_layers=2, dropout=0.5, nonlinearity='tanh',
+    #                bidirectional=True, batch_first=True)
+    # train(model, train_loader, val_loader, params['num_epochs'])
